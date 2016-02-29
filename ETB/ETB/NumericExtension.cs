2f15700091932f4b677fbea966850cc0b5963447
juna0613 <yuta.naito@gmail.com>
2016-02-29 10:34:56 +0900

    add cores

---
+++ ETB/ETB/NumericExtension.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ETB
+{
+    public static class NumericExtension
+    {
+        public static bool IsIn<T>(this T value, T start, T end, bool leftIncluded, bool rightIncluded) where T : IComparable
+        {
+            return (leftIncluded ? start.CompareTo(value) <= 0 : start.CompareTo(value) < 0) &&
+                   (rightIncluded ? value.CompareTo(end) <= 0 : value.CompareTo(end) < 0);
+        }
+        public static bool IsIn<T>(this T value, T start, T end) where T : IComparable
+        {
+            return value.IsIn(start, end, true, true);
+        }
+
+    }
+}
---
+++ ETB/ETB/TypeHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.Linq.Expressions;
+namespace ETB
+{
+    public static class TypeHelper
+    {
+        public static Func<T, IDictionary<string, string>> CreateMapFunc<T>(Func<object, string> stringifier)
+        {
+            var typ = typeof(T);
+            var props = typ.GetProperties();
+            var paramExp = Expression.Parameter(typ, "target");
+            var getters = props.Where(p => p.PropertyType.IsPrimitive).ToDictionary(prop => prop.Name, prop =>
+                {
+                    var getter = prop.GetGetMethod();
+                    var getterCallExp = Expression.Call(paramExp, getter);
+                    return Expression.Lambda<Func<T, string>>(getterCallExp, paramExp).Compile();
+                });
+
+            return t =>
+                {
+                    var dic = new Dictionary<string, string>();
+                    foreach (var g in getters)
+                    {
+                        dic[g.Key] = g.Value(t);
+                    }
+                    return dic;
+                };
+        }
+
+
+    }
+}
