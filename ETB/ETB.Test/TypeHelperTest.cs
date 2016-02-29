4a403eb777171e5604018bb2b32413926f0b3d91
juna0613 <yuta.naito@gmail.com>
2016-03-01 01:03:54 +0900

    add object version of mapper add test

---
+++ ETB/ETB.Test/TypeHelperTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+namespace ETB.Test
+{
+    public class MyClass
+    {
+        public string Data { get; set; }
+        public int Val { get; set; }
+        private DateTime PriDate { get; set; }
+    }
+    [TestFixture]
+    public class TypeHelperTest
+    {
+        [Test]
+        public void TestObj()
+        {
+            var dic = new Dictionary<string, object>();
+            dic["hoge"] = 1;
+
+        }
+        
+        [Test]
+        public void TestTypeHelper()
+        {
+            var mapper = TypeHelper.CreateObjMapFunc<MyClass>();
+            var data = new MyClass { Data = "soo", Val = 32 };
+            var ret = mapper(data);
+
+            Assert.That(ret.Keys.Count, Is.EqualTo(2));
+            Assert.That(ret["Data"], Is.EqualTo("soo"));
+            Assert.That(ret["Val"], Is.EqualTo(32));
+
+            var mapper2 = TypeHelper.CreateMapFunc<MyClass>();
+            var ret2 = mapper2(data);
+
+            Assert.That(ret2.Keys.Count, Is.EqualTo(2));
+            Assert.That(ret2["Data"], Is.EqualTo("soo"));
+            Assert.That(ret2["Val"], Is.EqualTo("32"));
+
+        }
+
+    }
+}
--- ETB/ETB/TypeHelper.cs
+++ ETB/ETB/TypeHelper.cs
@@ -8,29 +8,42 @@ namespace ETB
 {
     public static class TypeHelper
     {
-        public static Func<T, IDictionary<string, string>> CreateMapFunc<T>(Func<object, string> stringifier)
+        public static Func<T, IDictionary<string, string>> CreateMapFunc<T>()
+        {
+            return CreateMapFunc<T>((_, __) => __.ToString());
+        }
+        public static Func<T, IDictionary<string, string>> CreateMapFunc<T>(Func<string, object, string> stringifier)
+        {
+            var mapper = CreateObjMapFunc<T>();
+            return t =>
+            {
+                return mapper(t).ToDictionary(k => k.Key, v => stringifier(v.Key, v.Value));
+            };
+        }
+
+        public static Func<T, IDictionary<string, object>> CreateObjMapFunc<T>()
         {
             var typ = typeof(T);
             var props = typ.GetProperties();
             var paramExp = Expression.Parameter(typ, "target");
-            var getters = props.Where(p => p.PropertyType.IsPrimitive).ToDictionary(prop => prop.Name, prop =>
-                {
-                    var getter = prop.GetGetMethod();
-                    var getterCallExp = Expression.Call(paramExp, getter);
-                    return Expression.Lambda<Func<T, string>>(getterCallExp, paramExp).Compile();
-                });
+            var getters = props.Where(p => p.PropertyType.IsPrimitive || p.PropertyType == typeof(string) || p.PropertyType == typeof(DateTime)).ToDictionary(prop => prop.Name, prop =>
+            {
+                var getter = prop.GetGetMethod();
+                var getterCallExp = Expression.Call(paramExp, getter);
+                var castExp = Expression.Convert(getterCallExp, typeof(object));
+                return Expression.Lambda<Func<T, object>>(castExp, paramExp).Compile();
+            }).ToArray();
 
             return t =>
+            {
+                var dic = new Dictionary<string, object>();
+                foreach (var g in getters)
                 {
-                    var dic = new Dictionary<string, string>();
-                    foreach (var g in getters)
-                    {
-                        dic[g.Key] = g.Value(t);
-                    }
-                    return dic;
-                };
+                    dic[g.Key] = g.Value(t);
+                }
+                return dic;
+            };
         }
-
 
     }
 }
