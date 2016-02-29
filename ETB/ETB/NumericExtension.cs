0bdee2ae1f7042bbac16280d6d0e7c2eefab65b3
juna0613 <yuta.naito@gmail.com>
2016-02-29 10:35:50 +0900

    add default arguments to IsIn

--- ETB/ETB/NumericExtension.cs
+++ ETB/ETB/NumericExtension.cs
@@ -7,15 +7,11 @@ namespace ETB
 {
     public static class NumericExtension
     {
-        public static bool IsIn<T>(this T value, T start, T end, bool leftIncluded, bool rightIncluded) where T : IComparable
+        public static bool IsIn<T>(this T value, T start, T end, bool leftIncluded = true, bool rightIncluded = true) where T : IComparable
         {
             return (leftIncluded ? start.CompareTo(value) <= 0 : start.CompareTo(value) < 0) &&
                    (rightIncluded ? value.CompareTo(end) <= 0 : value.CompareTo(end) < 0);
         }
-        public static bool IsIn<T>(this T value, T start, T end) where T : IComparable
-        {
-            return value.IsIn(start, end, true, true);
-        }
-
+        
     }
 }
