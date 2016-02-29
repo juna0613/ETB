fb56c87f2809251d591b095cdb2093e129800acb
juna0613 <yuta.naito@gmail.com>
2016-02-29 11:18:41 +0900

    add basic IO loading component

---
+++ ETB/ETB.IO/ILoader.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+namespace ETB.IO
+{
+    public interface ILoader<T>
+    {
+        IEnumerable<T> Load();
+    }
+
+    public interface IDataLoader
+    {
+        DataTable Load();
+    }
+}
---
+++ ETB/ETB.IO/IWriter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+namespace ETB.IO
+{
+    public interface IWriter<T>
+    {
+        void Save(IEnumerable<T> data);
+    }
+
+    public interface IDataWriter
+    {
+        void Save(DataTable data);
+    }
+}
