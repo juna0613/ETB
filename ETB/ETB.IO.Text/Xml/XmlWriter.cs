de4a742b3c24ef118f0a8b7d5c807927e1ae66af
juna0613 <yuta.naito@gmail.com>
2016-03-01 16:39:16 +0900

    add xml loader/writer

---
+++ ETB/ETB.IO.Text/Xml/XmlWriter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace ETB.IO.Text.Xml
+{
+    public class XmlWriter<T> : IWriter<T>
+    {
+        private readonly TextWriter _writer;
+
+        public XmlWriter(TextWriter writer)
+        {
+            _writer = writer;
+        }
+
+        public void Save(IEnumerable<T> data)
+        {
+            var serializer = new System.Xml.Serialization.XmlSerializer(typeof(T[]));
+            serializer.Serialize(_writer, data.ToArray());
+        }
+    }
+}
